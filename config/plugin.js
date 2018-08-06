562d99e7477430da0028e8f5c748c9629285e594
xiongyang <yang.xiong@meicloud.com>
2018-08-06 20:57:20 +0800

    add-插件
    安装基础插件

--- config/plugin.js
+++ config/plugin.js
@@ -2,3 +2,18 @@
 
 // had enabled by egg
 // exports.static = true;
+
+exports.ejs = {
+  enable: true,
+  package: 'egg-view-ejs',
+};
+
+exports.redis = {
+  enable: true,
+  package: 'egg-redis',
+};
+
+exports.mongoose = {
+  enable: true,
+  package: 'egg-mongoose',
+};
